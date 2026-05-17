4ee8dd722fca3adb2e8efef0dd3d3f3c07824dbf
JorgeGuzman <jorguzman100@gmail.com>
2026-05-17 14:31:34 +0300

    01 Project setup - Java setup

---
+++ server/src/main/java/com/matchme/MatchMeApplication.java
@@ -0,0 +1,15 @@
+package com.matchme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchMeApplication {
+
+	public static void main(String[] args) {
+		
+		// Start the Spring backend
+		SpringApplication.run(MatchMeApplication.class, args);
+	}
+
+}
